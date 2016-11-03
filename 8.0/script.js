e83b11f40e11be47a569a9cb5cc30663ec22e755
medonohue <info@namistudios.com>
2016-11-03 18:58:08 -0400

    week 8 homework

--- 8.0/script.js
+++ 8.0/script.js
@@ -18,7 +18,7 @@ var airlines = d3.set();
 
 //Scale
 var scaleX = d3.scaleTime()
-    .range([0,w]);
+    .range([0,w]);//why doesn't it need a domain???
 var scaleColor = d3.scaleOrdinal()
     .range(['#fd6b5a','#03afeb','orange','#06ce98','blue']);
 var scaleY = d3.scaleLinear()
@@ -33,10 +33,12 @@ var axisY = d3.axisLeft()
     .scale(scaleY)
     .tickSize(-w);
 
+
+
 //Line generator
 var lineGenerator = d3.line()
-    .x(function(d){return scaleX(d.year)})
-    .y(function(d){return scaleY(d.value)})
+    .x(function(d){return scaleX(new Date(d.key) )})
+    .y(function(d){return scaleY(d.averagePrice)})
     .curve(d3.curveCardinal);
 
 d3.queue()
@@ -44,8 +46,8 @@ d3.queue()
     .await(function(err, data){
 
         //Mine the data to set the scales
-        scaleX.domain( d3.extent(data,function(d){return d.travelDate}) );
-        scaleColor.domain( airlines.values() );
+        scaleX.domain( d3.extent(data,function(d){return d.travelDate}) );//sets domain for x scale - why here instead of above?? finds lowest and highest dates
+        scaleColor.domain( airlines.values() );//what is airlines.values()?? it must come from airlines variable set above.
 
         //Add buttons
         d3.select('.btn-group')
@@ -59,12 +61,24 @@ d3.queue()
             .style('color','white')
             .style('background',function(d){return scaleColor(d)})
             .style('border-color','white')
+/*
             .on('click',function(d){
                 //Hint: how do we filter flights for particular airlines?
                 //data.filter(...)
 
                 //How do we then update the dots?
             });
+*/
+            .on('click',function(d){
+                //Hint: how do we filter flights for particular airlines?
+                //data.filter(...)
+				var dataFiltered = data.filter(function(e){return(d) == e.airline});
+				
+				draw(dataFiltered)
+				
+                //How do we then update the dots?
+            });
+
 
         //Draw axis
         plot.append('g').attr('class','axis axis-x')
@@ -72,6 +86,9 @@ d3.queue()
             .call(axisX);
         plot.append('g').attr('class','axis axis-y')
             .call(axisY);
+        
+        //Append <path>; this should only happen once
+        plot.append('path').attr('class','time-series');
 
         draw(data);
 
@@ -79,6 +96,9 @@ d3.queue()
 
 function draw(rows){
     //IMPORTANT: data transformation
+    
+    rows.sort(function(a,b){return a.travelDate - b.travelDate});
+    
     var flightsByTravelDate = d3.nest().key(function(d){return d.travelDate})
         .entries(rows);
 
@@ -89,7 +109,84 @@ function draw(rows){
     console.log(flightsByTravelDate);
 
     //Draw dots
+    
+    //UPDATE
+    var node = plot.selectAll('.flight')
+        .data(rows,function(d){return d.id});
 
+    var nodeEnter = node.enter()
+        .append('circle').attr('class','flight')
+        .merge(node)
+        .attr('r',3)
+        .attr('cx',function(d){return scaleX(d.travelDate)})
+        .attr('cy',function(d){return scaleY(d.price)})
+        .style('fill',function(d){return scaleColor(d.airline)})
+        .style('fill-opacity',.75) 
+    
+/*
+    //UPDATE
+    var node = plot.selectAll('.node')
+        .data(rows,function(d){return d.price});
+    //ENTER
+    var nodeEnter = node.enter()
+        .append('circle')
+        .attr('class','node')
+        .on('click',function(d,i){
+            console.log(d);
+            console.log(i);
+            console.log(this);
+        })
+*/
+        .on('mouseenter',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.select('.title')
+                .html(d.airline)
+            tooltip.select('.value')
+                .html('$' + d.price);
+
+            tooltip.transition().style('opacity',1);
+
+            d3.select(this).style('stroke-width','3px');
+        })
+        .on('mousemove',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            var xy = d3.mouse( d3.select('.container').node() );
+            tooltip
+                .style('left',xy[0]+40+'px')
+                .style('top',xy[1]-135+'px');
+        })
+        .on('mouseleave',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.transition().style('opacity',0);
+
+            d3.select(this).style('stroke-width','0px');
+        });
+    //UPDATE + ENTER
+    nodeEnter
+        .attr('cx',function(d){return scaleX(d.travelDate)})
+        .attr('cy',function(d){return scaleY(d.price)})
+        .merge(node)
+        .attr('r',4)
+        .transition()
+        .attr('cx',function(d){return scaleX(d.travelDate)})
+        .attr('cy',function(d){return scaleY(d.price)})
+        .style('fill',function(d){return scaleColor(d.airline)});
+    //EXIT
+    node.exit().remove();
+
+
+    //Draw path
+    var pathLine = plot.select('.time-series')
+        .datum(flightsByTravelDate)
+        .transition()
+        .attr('d',function(datum){
+            return lineGenerator(datum);
+        })
+        .style('fill','none')
+        .style('stroke-width','2px')
+        .style('stroke',function(datum){
+             return scaleColor(datum[0].values[0].airline);
+        });
 
     //Draw <path>
 }
@@ -108,3 +205,4 @@ function parse(d){
         id: d.id
     }
 }
+
